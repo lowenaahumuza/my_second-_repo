a1d7264ed38849fd0bbf000cad92a8447b9111f0
LULU LOWENA <relworx@Lulus-Macbook-Pro.local>
2024-09-18 10:06:17 +0300

    intial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int length = 8;
+    int width = 5;
+    double area;
+
+    area = length * width;
+
+    cout << "the area of a rectangle is " << area;
+    return 0;
+}
---
+++ work.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "heyyyyyy";
+
+
+    return 0;
+}
